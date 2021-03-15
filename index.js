e435a4634aeac4d32fa63bf15741c2ac3055a5f6
Carlj0666 <carlj1881@yahoo.com>
2021-03-14 17:17:14 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+  let giftCards = []
+  for (let i = 0; i < array.length; i++) {
+    giftCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+  return giftCards;
+}
+
+writeCards();
+
+function countDown(currentCount){
+  while(currentCount > 0) {
+  console.log(currentCount);
+  currentCount -= 1;
+  }
+  console.log(currentCount);
+}
